431bd76dedadb041657a1d36143e8159762adbe9
MattSlm <mahdi.soleimani@yale.edu>
2023-02-27 14:35:54 -0500

    added debugging fro badcasting error (3)

--- src/server/services/optimizer/OptimizerService.cpp
+++ src/server/services/optimizer/OptimizerService.cpp
@@ -146,8 +146,10 @@ void OptimizerService::runService()
             }
         }
         catch (std::exception &e) {
-            std::string stackTrace = panic::stack_dump(panic::stack_backtrace, panic::stack_backtrace_size);
-		    FTS3_COMMON_LOGGER_NEWLOG(DEBUG) << "Stacktrace: " << stackTrace << commit;
+            std::string stackTrace = fts3::common::panic::stack_dump(
+                        fts3::common::panic::stack_backtrace, 
+                        fts3::common::panic::stack_backtrace_size);
+		    FTS3_COMMON_LOGGER_NEWLOG(DEBUG) << "Stacktrace: " << stackTrace << fts3::common::commit;;
             FTS3_COMMON_LOGGER_NEWLOG(ERR) << "Process thread OptimizerService " << e.what() <<
             fts3::common::commit;
         }
